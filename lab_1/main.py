6953637106fc8ce9cdcacb46d4bb4ea2c78593a2
nastya24108 <43419787+nastya24108@users.noreply.github.com>
2018-09-23 21:25:15 +0300

    completed functions

--- lab_1/main.py
+++ lab_1/main.py
@@ -5,20 +5,60 @@ Count frequencies dictionary by the given arbitrary text
 """
 
 
-def calculate_frequences() -> dict:
-    """
-    Calculates number of times each word appears in the text
-    """
-   return d = {}
+def calculate_frequences(text: str) -> dict:
+    frequencies = {}
 
-def filter_stop_words() -> dict:
-    """
-    Removes all stop words from the given frequencies dictionary
-    """
-    pass
+    if text == None or text.isdigit():
+        return frequencies
+    elif not text:
+        return frequencies
+    elif type(text) == str:
+        for spam in text:
+            if spam in """1234567890-_+=()"*&?^:%$;№'#"@!`~:<>/\|].,}[{""":
+                text = text.replace(spam, ' ')
 
-def get_top_n() -> tuple:
-    """
-    Takes first N popular words
-    """
-    pass
+        split_text = text.lower()
+        split_text = text.split(' ')
+        
+        if '\n' in split_text:
+            while '\n' in split_text:
+                split_text.remove('\n')
+
+        for word in split_text:
+            quantity_words = split_text.count(word)
+            frequencies[word] = quantity_words
+            
+        return frequencies
+    else:
+        return frequencies
+
+
+
+def filter_stop_words(freq_dict: dict, stop_words: tuple) -> dict:
+    new_frequencies = frequencies.copy()
+
+    if stop_words == None or new_frequencies == None:
+        return new_frequencies
+
+    if stop_words:
+        for s_w in stop_words:
+            if s_w in new_frequencies:
+                new_frequencies.pop(s_w)
+
+    return new_frequencies
+
+def get_top_n(frequencies: dict, top_n: int) -> tuple:
+    if not top_n > 0:
+        return ()
+    else:
+        n = 0
+        top_n_list = []
+        for key in frequencies.keys():
+            if top_n == n:
+                break
+            else:
+                n = n + 1
+                top_n_list.append(key)
+
+    top_n_list = tuple(top_n_list)
+    return top_n_list
