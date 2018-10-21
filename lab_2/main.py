d23ff7c3dff07ea9253d78f491cbad331290d8e2
nastya24108 <43419787+nastya24108@users.noreply.github.com>
2018-10-21 20:13:47 +0300

    if freq is equal

--- lab_2/main.py
+++ lab_2/main.py
@@ -74,6 +74,10 @@ def choose_best(frequencies: dict, candidates: tuple) -> str:
             if int(frequencies[i]) > max_num:
                 max_num = int(frequencies[i])
                 max_num_word = i
+            if int(frequencies[i]) == max_num:
+                if max_num_word > word:
+                    max_num = int(frequencies[i])
+                    max_num_word = i
 
     return max_num_word
 
