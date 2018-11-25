ec9b1e34feb9132fa048e1ccdf1f9f05e49d9914
nastya24108 <43419787+nastya24108@users.noreply.github.com>
2018-11-25 19:20:54 +0300

    4.2 steps

--- lab_3/main.py
+++ lab_3/main.py
@@ -12,22 +12,62 @@ if __name__ == '__main__':
 
 
 class WordStorage:
+     def __init__(self):
+        self.storage = {}
+
     def put(self, word: str) -> int:
-        pass
+        id = 1
+        if word and type(word) == str:
+            if word not in self.storage.keys():
+                id = max(self.storage.values()) + 1
+                self.storage[word] = id
+            else:
+                self.storage[word] = id
+            return self.storage[word]
+        else:
+            return self.storage
 
     def get_id_of(self, word: str) -> int:
-        pass
+        if word and type(word) == str:
+            if word in self.storage.keys():
+                return self.storage[word]
+            else:
+                return None
 
     def get_original_by(self, id: int) -> str:
-        pass
+        if id and type(id) == int:
+            if id in self.storage.values():
+                for word in self.storage.keys():
+                    if id == self.storage[word]:
+                        return word
+         else:
+            return None
 
     def from_corpus(self, corpus: tuple):
-        pass
+        if corpus and type(corpus) == tuple:
+            WordStorage.put(self, word)
+        else:
+            return {}
 
 
 class NGramTrie:
+    def __init__(self, n):
+        self.size = n
+        self.gram_frequencies = {}
+        gram_log_probabilities = {}
+        
     def fill_from_sentence(self, sentence: tuple) -> str:
-        pass
+        if sentence and type(sentence) == tuple:
+            for i in range(len(sentence) - 1):
+                bi_gram = (sentence[i], sentence[i + 1])
+                if bi_gram not in self.gram_frequencies.keys():
+                    self.gram_frequencies[bi_gram] = 1
+                else:
+                    freq = self.gram_frequencies[bi_gram]
+                    self.gram_frequencies[bi_gram] = freq + 1
+            return 'OK'
+        else:
+            return 'ERROR'
 
     def calculate_log_probabilities(self):
         pass
@@ -37,9 +77,36 @@ class NGramTrie:
 
 
 def encode(storage_instance, corpus) -> list:
-    pass
+    if storage_instance and corpus:
+        list_of_id = []
+        for word in corpus:
+            list_of_id.append(WordStorage.get_id_of(self, word))
+        return list_of_id
 
 
 def split_by_sentence(text: str) -> list:
-    pass
-print('hello')
+    if not text:
+        return []
+
+    my_text = []
+    text = text.lower()
+    text = text.replace('\n', ' ')
+    if '  ' in text:
+        text = text.replace('  ', ' ')
+
+    import re
+    sentence = re.split(r'[.?!]', text)
+
+    for i in range(len(sentence)):
+        one_sent = ['<s>', ]
+        sentences = sentence[i].split()
+        word = ''
+        for el in sentences:
+            for el_el in
+                if el_el[1] in 'abcdefghijklmnopqrstuvwxyz':
+                    word += el_el[1]
+            if word:
+                one_sent.append(word)
+        one_sent.append('</s>')
+        my_text.append(one_sent)
+    return my_text
