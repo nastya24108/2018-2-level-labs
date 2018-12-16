87425409cd79253520a72402c745fb011597b0fd
nastya24108 <43419787+nastya24108@users.noreply.github.com>
2018-12-16 12:19:11 +0300

    the first variant

--- lab_4/main.py
+++ lab_4/main.py
@@ -9,24 +9,80 @@ if __name__ == '__main__':
 
 
 def clean_tokenize_corpus(texts: list) -> list:
-    pass
+    if not texts or not isinstance(texts, list):
+        return []
+    lst_corpus = []
+    for text in texts:
+        if text and isinstance(text, str):
+            lst_text = []
+            words = text.split()
+            for ind, el in enumerate(words):
+                el.lower()
+                if el.isalpha():
+                    lst_text.append(el)
+                else:
+                    clean_word = ''
+                    for element in el:
+                        if element.isalpha():
+                            clean_word += element
+                    if clean_word:
+                        lst_text.append(clean_word)
+            lst_corpus.append(lst_text)
 
 
 class TfIdfCalculator:
     def __init__(self, corpus):
-        pass
+        self.corpus = corpus
+        self.tf_values = []
+        self.idf_values = {}
+        self.tf_idf_values = []
 
     def calculate_tf(self):
-        pass
+        if self.corpus:
+            for text in self.corpus:
+                tf = {}
+                len_text = len(text)
+                for word in text:
+                    if word not in tf.keys():
+                        num = text.count(word)
+                        tf[word] = num / len_text
+                self.tf_values += [tf]
+        return self.tf_values
 
     def calculate_idf(self):
-        pass
+        if self.corpus:
+            dict_count_texts = {}
+            for text in self.corpus:
+                if not text:
+                    continue
+                word_list = []
+                for word in text:
+                    if word not in word_list:
+                        word_list.append(word)
+                for element in word_list:
+                    count_word_in_corpus = 0
+                    for another_text in self.corpus:
+                        if element in another_text:
+                            count_word_in_corpus += 1
+                    dict_count_texts[element] =count_word_in_corpus
+                    self.idf_values[element] = math.log(len(self.corpus / dict_count_texts.get(element)))
+            return self.idf_values
 
     def calculate(self):
-        pass
+        if self.tf_values and self.idf_values:
+            for elenent in self.tf_values:
+                tf_idf = {}
+                for word, tf in elenent.items():
+                    tf_idf[word] = tf * self.idf_values.get(word)
+                self.tf_idf_values += [tf_idf]
+        return self.tf_idf_values
 
     def report_on(self, word, document_index):
-        pass
+        if not self.tf_idf_values or document_index >= len(self.tf_idf_values):
+            return ()
+        tf_idf = self.tf_idf_values[document_index]
+        significance = sorted(tf_idf)
+        return tf_idf.get(word, significance.index(word))
 
 
 # scenario to check your work
